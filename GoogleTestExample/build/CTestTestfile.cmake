f3690a0d64b4c1acacc6add0e84493ee799ee7ad
Boris Minaev <qwerty787788@qwerty787788.(none)>
2013-03-30 02:19:02 +0400

    Test commit

---
+++ GoogleTestExample/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/qwerty787788/cpp/geometry/GoogleTestExample/src
+# Build directory: /home/qwerty787788/cpp/geometry/GoogleTestExample/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(GoogleTest "/home/qwerty787788/cpp/geometry/GoogleTestExample/build/GoogleTest")
